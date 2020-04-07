508ad5574a0a84a8a8fafbb8db7df54439082770
S3bb1 <sebastian.dechant@evan.team>
2020-04-07 17:04:46 +0200

    root sync

--- syncWatch.sh
+++ syncWatch.sh
@@ -5,7 +5,7 @@ syncing=true
 while [[ $syncing = true ]]
 do
     curlResult=$(curl --retry 300 --retry-delay 5 -s --data '{"method":"eth_syncing","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:8545)
-    synced=$(echo $curlResult | jq '.result.currentBlock == "0x73F26"')
+    synced=$(echo $curlResult | jq '.result.currentBlock == "0x73DB9"')
     if $synced; then
       break
     fi
